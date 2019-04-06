6d94592e66e46953faf14607ab310977cad3761d
Rom1 <rom1@canel.ch>
2019-04-06 18:10:53 +0200

    Ajouter la lecture des boutons.

--- simon.ino
+++ simon.ino
@@ -1,28 +1,99 @@
-#define LED1 D0
-#define LED2 D1
-#define LED3 D2
-#define LED4 D3
+#define LED_PIN1 D0
+#define LED_PIN2 D1
+#define LED_PIN3 D2
+#define LED_PIN4 D3
+
+#define SW_PIN1 D5
+#define SW_PIN2 D6
+#define SW_PIN3 D7
+#define SW_PIN4 D8
+
+enum{
+  LED1 = 0,
+  LED2,
+  LED3,
+  LED4
+};
+
+#define DELAYON 500
+
+void ledON(int);
+void ledsON(int);
+int readButtons(void);
 
 void setup(void)
 {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Coucou");
+
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
+  pinMode(LED_PIN3, OUTPUT);
+  pinMode(LED_PIN4, OUTPUT);
+
+  pinMode(SW_PIN1, INPUT);
+  pinMode(SW_PIN2, INPUT);
+  pinMode(SW_PIN3, INPUT);
+  pinMode(SW_PIN4, INPUT);
 }
 
 void loop(void)
 {
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, HIGH);
-  digitalWrite(LED3, HIGH);
-  digitalWrite(LED4, HIGH);
-  delay(1000);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
-  digitalWrite(LED3, LOW);
-  digitalWrite(LED4, LOW);
-  delay(1000);
+  ledsON(readButtons());
+}
+
+void ledsON(int led)
+{
+  switch(led)
+  {
+    case LED1:
+      ledON(LED_PIN1);
+      break;
+    case LED2:
+      ledON(LED_PIN2);
+      break;
+    case LED3:
+      ledON(LED_PIN3);
+      break;
+    case LED4:
+      ledON(LED_PIN4);
+      break;
+  }
+}
+
+void ledON(int led)
+{
+  digitalWrite(led, HIGH);
+  delay(DELAYON);
+  digitalWrite(led, LOW);
+}
+
+int readButtons(void)
+{
+  while(1)
+  {
+    if(digitalRead(SW_PIN1))
+    {
+      delay(300);
+      return LED1;
+    }
+    if(digitalRead(SW_PIN2))
+    {
+      delay(300);
+      return LED2;
+    }
+    if(digitalRead(SW_PIN3))
+    {
+      delay(300);
+      return LED3;
+    }
+    if(digitalRead(SW_PIN4))
+    {
+      delay(300);
+      return LED4;
+    }
+    delay(10);
+  }
 }
 
 
