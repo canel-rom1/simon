a03504ac123947b4bfa0d4f6869862dee863fa0a
Rom1 <rom1@canel.ch>
2019-04-06 16:37:03 +0200

    Allumer les LEDs

--- simon.ino
+++ simon.ino
@@ -1,11 +1,28 @@
+#define LED1 D0
+#define LED2 D1
+#define LED3 D2
+#define LED4 D3
+
 void setup(void)
 {
-
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
 }
 
 void loop(void)
 {
-
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  delay(1000);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  delay(1000);
 }
 
 
